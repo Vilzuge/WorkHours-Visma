6b9818ee8e7f33c0f84e25b3ca446fefd7eb04a6
Vilzuge <ville.martas@hotmail.com>
2021-03-09 14:16:37 +0200

    first commmit

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿//Ville Martas
+//09.03.2020
+
+using System;
+
+namespace ValidationVisma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shift Monday = new Shift(new DateTime(2021, 3, 8), "07:30", "15:00");
+            Shift Tuesday = new Shift(new DateTime(2021, 3, 9), "07:00", "16:30");
+            Shift Wednesday = new Shift(new DateTime(2021, 3, 10), "07:15", "15:45");
+
+            Console.WriteLine(Monday.DayLength);
+            Console.WriteLine(Tuesday.DayLength);
+            Console.WriteLine(Wednesday.DayLength);
+
+            //For testing the new class
+            while (true)
+            {
+                Console.WriteLine("\nTesting the new Shift class");
+                Console.WriteLine("1) Create a new shift and output the hours");
+                Console.WriteLine("0) Exit");
+                Console.Write(">> ");
+                string choise = Console.ReadLine();
+                switch (choise)
+                {
+                    case "1":
+                        Console.Write("Give a year: ");
+                        string year = Console.ReadLine();
+                        Console.Write("Give a month: ");
+                        string month = Console.ReadLine();
+                        Console.Write("Give a day: ");
+                        string day = Console.ReadLine();
+
+                        Console.Write("Give a starting time (hh:mm): ");
+                        string start = Console.ReadLine();
+                        Console.Write("Give an ending time (hh:mm): ");
+                        string end = Console.ReadLine();
+
+                        try
+                        {
+                            Shift Today = new Shift(new DateTime(int.Parse(year), int.Parse(month), int.Parse(day)), start, end);
+                            Console.WriteLine("Length of this shift is: " + Today.DayLength.ToString() + " hours.");
+                        }
+                        catch (SystemException e)
+                        {
+                            Console.WriteLine(e);
+                        }
+                        break;
+
+                    case "0":
+                        System.Environment.Exit(1);
+                        break;
+
+                    default:
+                        Console.WriteLine("Not a valid choise, choose again.");
+                        break;
+                }
+
+            }
+        }
+    }
+}
---
+++ Shift.cs
@@ -0,0 +1,53 @@
+﻿//Ville Martas
+//09.03.2020
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValidationVisma
+{
+    public class Shift
+    {
+        private DateTime date;
+        private string start, end;
+        private decimal hourStart, hourEnd, hours;
+        private decimal minuteStart, minuteEnd, minutes;
+        private decimal total;
+
+        public Shift(DateTime aDate, string aStart, string aEnd)
+        {
+            date = aDate;
+            start = aStart;
+            end = aEnd;
+
+            string[] startTimes = start.Split(':');
+            string[] endTimes = end.Split(':');
+
+            hourStart = decimal.Parse(startTimes[0]);
+            hourEnd = decimal.Parse(endTimes[0]);
+            if (hourStart < 0 || hourStart > 23 || hourEnd < 0 || hourEnd > 23)
+                throw new ArgumentException("Individual hours out of bounds. Possible values for hours are 00-23.");
+
+            minuteStart = decimal.Parse(startTimes[1]);
+            minuteEnd = decimal.Parse(endTimes[1]);
+            if (minuteStart < 0 || minuteStart > 59 || minuteEnd < 0 || minuteEnd > 59)
+                throw new ArgumentException("Individual minutes out of bounds. Possible values for minutes are 00-59");
+
+            hours = hourEnd - hourStart;
+            minutes = (minuteEnd - minuteStart) / 60; //can be negative!
+
+            if (hours < 0)
+                throw new ArgumentException("Hours can not be negative. Shift must start before it ends.");
+
+            total = hours + minutes;
+            if (total < 0 || total > 16)
+                throw new ArgumentException("Shifts length minimum is 0 and maximum 16 hours.");
+        }
+
+        public decimal DayLength
+        {
+            get { return total; }
+        }
+    }
+}
